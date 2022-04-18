8e82c45b0eb6ccc9fb93edaf2ae895efbbc050af
Lyric Harrison <lharrison@diesellaptops.com>
2022-04-18 14:46:23 -0400

    section 17 done now in section 18

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,8 +1,20 @@
-@error('title')
-    <div style="color:red">{{ $message }}</div>
-@enderror
-<div><input type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}"></div>
-@error('content')
-    <div style="color:red">{{ $message }}</div>
-@enderror
-<div><textarea name="content">{{ old('content', optional($post ?? null)->content) }}</textarea></div>
+<div class="form-group">
+    @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <div>
+        <label for="title">Title</label>
+        <input id="title" type="text" name="title" class="form-control" value="{{ old('title', optional($post ?? null)->title) }}">
+    </div>
+    @error('content')
+        <div class="alert alert-danger my-3">
+            {{ $message }}
+        </div>
+    @enderror
+    <div class="form-group">
+        <label for="content">Content</label>
+        <textarea class="form-control" id="content" name="content">
+            {{ old('content', optional($post ?? null)->content) }}
+        </textarea>
+    </div>
+</div>
