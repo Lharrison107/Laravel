3fd05cdaea529e316c2cf7908705e63d06be6f3c
Lyric Harrison <lharrison@diesellaptops.com>
2022-04-21 17:22:35 -0400

    working on making the factories and the seeders. outside research for updated code

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usersCount = max((int)$this->command->ask('How many users would you like?', 20), 1);
+        // factory(App\User::class)->states('john-doe')->create();
+        // factory(App\User::class, $usersCount)->create();
+    }
+}
