4a06a79b9b5ebcab518afc81d638ee396569c005
Lharrison107 <103782222+Lharrison107@users.noreply.github.com>
2022-04-28 10:11:39 -0400

    cant get policies to work. continueing with gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\BlogPost;
+// use App\Models\BlogPost;
 use App\Policies\BlogPostPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -16,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        BlogPost::class => BlogPostPolicy::class,
+        // 'App\Model\BlogPost' => 'App\Policies\BlogPostPolicy'
     ];
 
     /**
@@ -28,35 +28,35 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        // Gate::define('update-post', function ($user, $post)
-        // {
-        //    return $user->id == $post->user_id;
-        // });
+        Gate::define('update-post', function ($user, $post)
+        {
+           return $user->id == $post->user_id;
+        });
 
-        // Gate::define('delete-post', function ($user, $post)
-        // {
-        //    return $user->id == $post->user_id;
-        // });
+        Gate::define('delete-post', function ($user, $post)
+        {
+           return $user->id == $post->user_id;
+        });
 
         // Gate::define('posts-update', 'App\Policies\BlogPostPolicy@update');
 
         // Gate::define('posts-delete', 'App\Policies\BlogPostPolicy@delete');
 
-        Gate::resource('posts', BlogPostPolicy::class);
+        // Gate::resource('posts', BlogPostPolicy::class);
 
-        // Gate::before(function ($user, $ability)
-        // {
-        //    if ($user->is_admin && in_array($ability, ['posts-update', 'posts-delete'])) {
-        //        return true;
-        //    };
-        // });
+        Gate::before(function ($user, $ability)
+        {
+           if ($user->is_admin && in_array($ability, ['posts-update', 'posts-delete'])) {
+               return true;
+           };
+        });
 
-        // Gate::after(function ($user, $ability, $result)
-        // {
-        //    if ($user->is_admin) {
-        //        return true;
-        //    };
-        // });
+        Gate::after(function ($user, $ability, $result)
+        {
+           if ($user->is_admin) {
+               return true;
+           };
+        });
 
     }
 }
