7fac7c6edf07c9ab20a49ae2ffc57f4d4a9718e4
Lharrison107 <103782222+Lharrison107@users.noreply.github.com>
2022-04-28 17:34:30 -0400

    part 148 complete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,6 +41,7 @@ class PostController extends Controller
                 'posts' => BlogPost::Latest()->withCount('comments')->get(),
                 'mostCommented' => BlogPost::mostCommented()->take(5)->get(),
                 'mostActive' => User::MostBlogPosts()->take(5)->get(),
+                'mostActiveLastMonth' => User::WithMostBlogPostsLastMonth()->take(5)->get(),
             ]
         );
     }
