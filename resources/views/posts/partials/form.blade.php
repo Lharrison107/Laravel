f985af430fb466c2286b556d92f3dc6eff68cf30
Lharrison107 <103782222+Lharrison107@users.noreply.github.com>
2022-05-10 13:36:26 -0400

    section 32 part 182 complete

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,22 +1,27 @@
+
 <div class="form-group">
-    {{-- @error('title')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror --}}
-    <div>
+    <strong>
         <label for="title">Title</label>
-        <input id="title" type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title', optional($post ?? null)->title) }}">
-        <x-errors :errors="$errors->first('title')" />
-    </div>
-    {{-- @error('content')
-        <div class="alert alert-danger my-3">
-            {{ $message }}
-        </div>
-    @enderror --}}
-    <div class="form-group">
+    </strong>
+    <input id="title" type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title', optional($post ?? null)->title) }}">
+    <x-errors :errors="$errors->first('title')" />
+</div>
+
+<div class="form-group">
+    <strong>
         <label for="content">Content</label>
-        <textarea class="form-control {{ $errors->has('content') ? 'is-invalid' : '' }}" id="content" name="content">
-            {{ old('content', optional($post ?? null)->content) }}
-        </textarea>
-        <x-errors :errors="$errors->first('content')" />
-    </div>
+    </strong>
+    <textarea class="form-control {{ $errors->has('content') ? 'is-invalid' : '' }}" id="content" name="content">
+        {{ old('content', optional($post ?? null)->content) }}
+    </textarea>
+    <x-errors :errors="$errors->first('content')" />
+</div>
+
+<div class="form-group">
+    <strong>
+        <label>Thumbnail</label>
+    </strong>
+    
+</br>
+    <input type="file" name="thumbnail" class="form-control-file"/>
 </div>
