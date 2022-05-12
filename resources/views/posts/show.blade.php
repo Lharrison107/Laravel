dfa0c74565ee9e6cf74fd934a6c44d4edf567635
Lharrison107 <103782222+Lharrison107@users.noreply.github.com>
2022-05-12 16:13:46 -0400

    part 188 complete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,16 +5,36 @@
 @section('content')
     <div class="row">
         <div class="col-8">
-            <h1>
+            @if ($post->image)
+                <div style="background-image: url('{{ $post->image->url() }}'); min-height: 500px; color: white; text-align: center; background-attachment: fixed; ">
+                    <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+            @else
+
+                <h1>
+
+            @endif
+
                 {{ $post->title }}
                 <x-badge :show="now()->diffInMinutes($post->created_at) < 20">
                     New Blog Post!
                 </x-badge>
-            </h1>
+
+            @if ($post->image)
+
+                    </h1>
+                </div>
+                
+            @else
+
+                 </h1>
+
+            @endif
+
+           
 
             <p> {{ $post->content }} </p>
 
-            <img src="{{ $post->image->url() }}" />
+            {{-- <img src="{{ $post->image->url() }}" /> --}}
             {{-- @dump( $post->image->url()) --}}
 
             <x-updated :date="$post->created_at" :name="$post->user->name"/>
